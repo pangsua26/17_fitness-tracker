5b8e53c533aae82d579b2d0ae6a4f07ca6c2151c
Pangsua Vang <pangsua26@yahoo.com>
2020-07-30 17:36:40 -0500

    get wo in range

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -13,4 +13,16 @@ module.exports = function(app) {
             });
     });
 
+    // get workouts in range
+    app.get("/api/workouts/range", (req, res) => {
+        Workout.find({})
+            .sort({ day: -1 })
+            .limit(7)
+            .then(dbWorkouts => {
+                res.json(dbWorkouts)
+            })
+            .catch(err => {
+                res.status(400).json(err)
+            });
+    });
 }
