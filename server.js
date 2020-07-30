ca60b8718a76e64efc28c1f805673db9966571da
Pangsua Vang <pangsua26@yahoo.com>
2020-07-30 17:11:59 -0500

    mongoose and listen

--- server.js
+++ server.js
@@ -4,3 +4,19 @@ const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// routes
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port http://localhost:${PORT}`);
+});
