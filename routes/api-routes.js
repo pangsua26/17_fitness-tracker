8c718791c2b1f850856a1c21fdec62548fbfc88f
Pangsua Vang <pangsua26@yahoo.com>
2020-07-30 17:36:06 -0500

    get workout

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,1 +1,16 @@
 const Workout = require("../models/workout.js");
+
+module.exports = function(app) {
+    //get last workout
+    app.get("/api/workouts", (req, res) => {
+        console.log("success")
+        Workout.find({})
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.status(400).json(err)
+            });
+    });
+
+}
